3e9b8de7c87dd6a4a0e8822273692b02da189b31
Dinesh <dadshahidah@gmail.com>
2025-03-17 04:43:10 +0800

    First Development of Main Page and Nav also Card

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Muhammad Zulfaqar Portfolio",
+  description: "Software Dev | Software Engineer | Fullstack Developer",
+};
